b2e00d60737313c2087f0fc22cec0f1204049b8c
Truong Giang <giang.bui@fssc.com.vn>
2024-04-07 19:08:16 +0700

    Revert "no message"
    This reverts commit 11a030768e435d736fd45a8f79f757113aa556b0.

--- Database/5. createJob.sql
+++
@@ -1,9 +0,0 @@
-BEGIN
-    DBMS_SCHEDULER.create_job (
-    job_name => 'AUTO_CALCULATE_SALARY',
-    job_type => 'PLSQL_BLOCK',
-    job_action => 'BEGIN job_auto_calculate_salary;END;',
-    start_date => CURRENT_TIMESTAMP,
-    repeat_interval => 'FREQ=DAILY; BYHOUR=0',
-    enabled => TRUE);
-END;
